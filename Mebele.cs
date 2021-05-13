aac9d8522a9a2cdfc260b159cfc7a326af4f0442
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-13 21:03:30 +0300

    Add project files.

---
+++ Mebele.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace day12_oop_abstract_class
+{
+    //bāzes klase !
+    //no šīs klases atvasināsim apakšklases.
+    abstract class Mebele
+    {
+        public Mebele()
+        {
+            info = new string[4];
+            info[0] = "Nosaukums: ";
+            info[1] = "Ražotājs: ";
+            info[2] = "Izcelsmes Valsts: ";
+            info[3] = "Info: ";
+        }
+
+        public void SetCena(double price)
+        {
+            cena = price;
+        }
+
+        //virtuāla metode, kuru mēs pārrakstīsim atšķirīgi, atkarībā no tā, 
+        //kas tā būs par atvasināto klasi un kādas būs vajadzības
+        //public virtual void SetInfo()
+        //{
+        //  kods
+        //  kods
+        //  kods
+        //  kods
+        //}
+
+
+        //varbūt bāzes klasē šim kodam metodē nav jēgas
+        //abstraktai metodei bāzes klasē nevar būt ķermenis (kods)
+
+        //idejiski šai metodei ir jēga, bet tikai atvasinātajā klasē
+        //pieprasu, lai visām atvasinātajā klasē būtu šī metode implementēta
+        //klasei jābūt abstraktai !
+        public abstract void SetInfo();
+
+
+
+        public void SetDati(string[] dati)
+        {
+            info = dati;
+        }
+
+
+
+        string[] info;
+        double cena;
+        Color krasa;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace day12_oop_abstract_class
+{
+
+    class Galds : Mebele
+    {
+        public Galds()
+        {
+        }
+
+
+        //parrakstam bāzes klases metodi - lai pielāgotu savām vajadzībām
+        
+        public override void SetInfo()
+        {
+            string[] info = new string[4];
+            info[0] = "Nosaukums: Galds";
+            info[1] = "Ražotājs: ???";
+            info[2] = "Izcelsmes Valsts: Ķīna ";
+            info[3] = "Info: <NAV>";
+
+            kaju_skaits = 4;
+        }
+        
+
+        int kaju_skaits;
+    }
+    class GrozamsKresls : Mebele
+    {
+
+        public GrozamsKresls()
+        {
+        }
+
+        public override void SetInfo()
+        {
+            string[] info = new string[4];
+            info[0] = "Nosaukums: GrozamsKresls";
+            info[1] = "Ražotājs: ???";
+            info[2] = "Izcelsmes Valsts: Ķīna ";
+            info[3] = "Info: <NAV>";
+
+            kaju_skaits = 1;
+        }
+
+        int kaju_skaits;
+
+    }
+    class Tafele : Mebele
+    {
+        bool pie_sienas_liekama;
+
+        public Tafele()
+        {
+        }
+
+        public override void SetInfo()
+        {
+            string[] info = new string[4];
+            info[0] = "Nosaukums: Tafele";
+            info[1] = "Ražotājs: ???";
+            info[2] = "Izcelsmes Valsts: Ķīna ";
+            info[3] = "Info: <NAV>";
+            pie_sienas_liekama = true;
+        }
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //izveidojiet atsevišķā failā bāzes klasi tā, lai varētu radīt atvasinātas klases
+            //klasei jābūt jēgpilnai
+
+            //bāzes klase satur :
+            //1 vai 2 laukus
+            //1 vai 2 metodes
+
+
+
+            //nav iespējas instancēt abstraktu klasi !
+            //Mebele m = new Mebele();
+            //m.SetInfo();
+            
+
+            Galds g = new Galds();
+            g.SetInfo();
+
+
+            GrozamsKresls gr = new GrozamsKresls();
+            gr.SetInfo();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
